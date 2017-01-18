645b8ba4e222df42b89eea52384c6a6242b6aa0e
Sam Messina <samuel.messina@gmail.com>
2017-01-18 12:05:16 -0600

    Added contact section

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,9 +1,23 @@
 import React    from 'react';
+import {Center} from '../styles';
 
 class Contact extends React.Component {
     render() {
         return (
-            <h2>Contact Section</h2>
+            <div className="row">
+                <div className="col-xs-12" style={Center}>
+                    <h2>Get in touch.</h2>
+                    <p>
+                        <i className="fa fa-envelope"></i>
+                        sam@smessina.com
+                    </p>
+                    <p>
+                        <i className="fa fa-github"></i> 
+                        github.com/regexpressyourself
+                    </p>
+                </div>
+            </div>
+
         )
     }
 }
