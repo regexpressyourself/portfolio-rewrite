a05787c342cffb4db207a1ae0782d47ba4ec91bf
Sam Messina <samuel.messina@gmail.com>
2017-01-19 16:37:34 -0600

    Updated project section calls

--- app/components/ProjectDescription.js
+++ app/components/ProjectDescription.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {ProjectRowItem,
+        SmallProjectRowItem} from '../styles';
 
 class ProjectDescription extends React.Component {
     constructor(props) {
@@ -6,12 +8,17 @@ class ProjectDescription extends React.Component {
     }
     render() {
         return (
-            <div className="col-sm-8">
+            <div className="col-sm-6 hidden-xs" style={ProjectRowItem}>
                 <h3>{this.props.title}</h3>
                 <p>
                     {this.props.description}
-
                 </p>
+                <span className="clickable">
+                    <i className="fa fa-link"></i> Link
+                </span>
+                <span className="clickable">
+                    <i className="fa fa-github"></i> Github
+                </span>
             </div>
         )
     }
