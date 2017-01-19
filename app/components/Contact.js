0717162b83924d573f03d9a58aa03d243460121e
Sam Messina <samuel.messina@gmail.com>
2017-01-19 16:37:20 -0600

    Updated contact and footer section presentation

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,18 +1,20 @@
 import React    from 'react';
-import {Center} from '../styles';
+import {Center,
+        StaticWidth} from '../styles';
 
 class Contact extends React.Component {
     render() {
         return (
             <div className="row">
                 <div className="col-xs-12" style={Center}>
-                    <h2>Get in touch.</h2>
-                    <p>
+                    <h2 style={Center}>Get in touch.</h2>
+                    <p style={StaticWidth} className="clickable">
                         <i className="fa fa-envelope"></i>
                         sam@smessina.com
                     </p>
-                    <p>
-                        <i className="fa fa-github"></i> 
+                    <br/>
+                    <p style={StaticWidth} className="clickable">
+                        <i className="fa fa-github"></i>
                         github.com/regexpressyourself
                     </p>
                 </div>
